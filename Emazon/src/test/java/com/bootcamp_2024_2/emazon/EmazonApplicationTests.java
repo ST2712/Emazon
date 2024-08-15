4d6a4a5654692d78773333710942bce285c9831a
ST2712 <2220211016@estudiantesunibague.edu.co>
2024-08-15 10:42:58 -0500

    First commit

---
+++ Emazon/src/test/java/com/bootcamp_2024_2/emazon/EmazonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bootcamp_2024_2.emazon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmazonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
